b4aec24a0b646b593662fb95815bfc3f68e3e36e
mpastore90 <mpastore90@users.noreply.github.com>
2023-03-03 21:28:59 +0100

    initialise spring boot project

---
+++ src/test/java/guru/spring5/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring5.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
